f8acd0570649cc80738e5d73832171dbe46dacb2
noal <noaltessman@gmail.com>
2019-04-11 18:00:08 -0600

    random image function, api connection, and help page added

--- discordtest/index.js
+++ discordtest/index.js
@@ -1,16 +1,31 @@
 const Discord = require('discord.js');
 const { prefix, token} = require('../../../noal/config.json');
-const client = new Discord.Client();
-const spongebob = client.emojis.get("565285907566034967");
+const client = new Discord.Client({disableEveryone:true});
+const axios = require('axios');
+//FUNCTIONS//
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
+//Broken function, doesn't work in time.
 
+// function randImage(){
+//     axios.get('https://picsum.photos/200/300/?random')
+//         .then(function(response){
+//             var image = response.request.path;
+//             image= image.split('');
+//             image=image.splice(16,image.length)
+//             image=image.join('').toString();
+//             return`https://picsum.photos/${image}.jpg`
+//         }).catch(function(error){
+//             console.log(error)
+//         })
+// }
 
+//onReady//
 client.once('ready',() =>{
     console.log("ready");
 })
-
+//MESSAGE COMMANDS//
 client.on('message', message =>{
         if (message.content == "aliun"){
             message.channel.send('ayy lmao');
@@ -25,8 +40,19 @@ client.on('message', message =>{
             }
             mock = mock.join("").toString();
             message.channel.send("<:spongebob:565285907566034967>"+mock+"<:spongebob:565285907566034967>")
-        } else if(message.content.startsWith('test')){
-
+        } else if(message.content.startsWith('randomImage')){
+            axios.get('https://picsum.photos/400/600/?random')
+                .then(function(response){
+                     var image = response.request.path;
+                     image= image.split('');
+                     image=image.splice(16,image.length)
+                     image=image.join('').toString();
+                      message.channel.send({files:[`https://picsum.photos/${image}.jpg`]})
+                }).catch(function(error){
+            console.log(error)
+                    })
+        } else if (message.content.startsWith('nord help')){
+            message.author.send('Commands: aliun (will say ayy lmao), randomImage(will display random image from online API), mock <insert sentence here> will change text to spongebob mocking text')
         }
     })
 
