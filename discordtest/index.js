e9a4df63faf1b12b312186eaa66fa970dbe3320a
noal <noaltessman@gmail.com>
2019-04-15 11:53:14 -0600

    small updates

--- discordtest/index.js
+++ discordtest/index.js
@@ -42,18 +42,19 @@ client.on('message', message =>{
             message.channel.send("<:spongebob:565285907566034967>"+mock+"<:spongebob:565285907566034967>")
         } else if(message.content.startsWith('randomImage')){
             axios.get('https://picsum.photos/400/600/?random')
-                .then(function(response){
+                .then((response) =>{
                      var image = response.request.path;
                      image= image.split('');
                      image=image.splice(16,image.length)
                      image=image.join('').toString();
-                      message.channel.send({files:[`https://picsum.photos/${image}.jpg`]})
+                      message.channel.send({files:[`https://picsum.photos/${image}.jpg`]});
+                      console.log("image sent to channel "+message.channel.guild.name)
                 }).catch(function(error){
             console.log(error)
                     })
         } else if (message.content.startsWith('nord help')){
             message.author.send('Commands: aliun (will say ayy lmao), randomImage(will display random image from online API), mock <insert sentence here> will change text to spongebob mocking text')
-        }
+        } 
     })
 
 client.login(token);
